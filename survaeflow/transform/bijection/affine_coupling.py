f49606c27c65e0c4724a52cda107553978168a5b
revsic <revsic99@gmail.com>
2021-01-20 22:35:08 +0900

    affine coupling: Add affine coupling bijection

---
+++ survaeflow/transform/bijection/affine_coupling.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+
+from .. import Transform
+
+
+class AffineCoupling(Transform):
+    """Affine coupling layer.
+    """
+    def __init__(self, encoder, axis=-1):
+        """Initializer.
+        Args:
+            encoder: tf.keras.Model, affine parameter encoder.
+            axis: int, target axis.
+        """
+        super(AffineCoupling, self).__init__()
+        self.encoder = encoder
+        self.axis = axis
+
+    def call(self, inputs):
+        """Affine coupling bijection and compute log-determinant.
+        Args:
+            inputs: tf.Tensor, [tf.float32; [B, ...]], input tensor.
+        Returns:
+            z: tf.Tensor, [tf.float32; [B, ...]], latent.
+            ldj: tf.Tensor, [tf.float32; [B]], log-determinant of jacobian.
+        """
+        # [B, ..., C // 2, ...], [B, ..., C // 2, ...]
+        x1, x2 = tf.split(inputs, 2, axis=self.axis)
+        # [B, ..., C // 2, ...], [B, ..., C // 2, ...]
+        mu, std = self.encoder(x2)
+        # [B, ..., C // 2, ...]
+        x1 = x1 * std + mu
+        # [B, ...]
+        z = tf.concat([x1, x2], axis=self.axis)
+        # [B]
+        ldj = tf.reduce_sum(tf.log(std), axis=tf.shape(std)[1:])
+        return z, ldj
+
+    def forward(self, inputs):
+        """Affine coupling bijection.
+        Args:
+            inputs: tf.Tensor, [tf.float32; [B, ...]], input tensor.
+        Returns:
+            tf.Tensor, [tf.float32; [B, ...]], latent.
+        """
+        # [B, ..., C // 2, ...], [B, ..., C // 2, ...]
+        x1, x2 = tf.split(inputs, 2, axis=self.axis)
+        # [B, ..., C // 2, ...], [B, ..., C // 2, ...]
+        mu, std = self.encoder(x2)
+        # [B, ..., C // 2, ...]
+        x1 = x1 * std + mu
+        # [B, ...]
+        return tf.concat([x1, x2], axis=self.axis)
+    
+    def inverse(self, inputs):
+        """Inverting affine coupling.
+        Args:
+            inputs: tf.Tensor, [tf.float32; [B, ...]], latent.
+        Returns:
+            tf.Tensor, [tf.float32; [B, ...]], recovered sample.
+        """
+        # [B, ..., C // 2, ...], [B, ..., C // 2, ...]
+        z1, z2 = tf.split(inputs, 2, axis=self.axis)
+        # [B, ..., C // 2, ...]
+        x2 = z2
+        # [B, ..., C // 2, ...]
+        mu, std = self.encoder(x2)
+        # [B, ..., C // 2, ...]
+        x1 = (z1 - mu) / std
+        # [B, ..., C // 2, ...]
+        return tf.concat([x1, x2], axis=self.axis)
