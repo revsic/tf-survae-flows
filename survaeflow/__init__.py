36a58f7b7e3994e28f80daa74f90feb9efcbe0ba
revsic <revsic99@gmail.com>
2021-01-20 22:05:11 +0900

    flow: Add invertible flow

---
+++ survaeflow/__init__.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+
+
+class Flow(tf.keras.Model):
+    """Invertible flow.
+    """
+    def __init__(self, networks):
+        """Initializer.
+        Args:
+            networks: List[tf.keras.Model], sequence of transforms.
+        """
+        super(Flow, self).__init__()
+        self.networks = networks
+    
+    def call(self, inputs):
+        """Generate latent and compute log-determinant.
+        Args:
+            inputs: tf.Tensor, [tf.float32; [B, ...]], input tensor.
+        Returns:
+            z: tf.Tensor, [tf.float32; [B, ...]], latent tensor.
+            ldj: tf.Tensor, [tf.float32; [B]], log-determinant of jacobian
+                (log-likelihood contribution term).
+        """
+        z, ldj = inputs, 0.
+        for net in self.networks:
+            z, contrib = net(z)
+            ldj = ldj + contrib
+        return z, ldj
+
+    def forward(self, inputs):
+        """Generate latent.
+        Args:
+            inputs: tf.Tensor, [tf.float32; [B, ...]], input tensor.
+        Returns:
+            z: tf.Tensor, [tf.float32; [B, ...]], latent tensor.
+        """
+        z = inputs
+        for net in self.networks:
+            z = net.forward(z)
+        return z
+
+    def inverse(self, latent):
+        """Generate sample from latent.
+        Args:
+            latent: tf.Tensor, [tf.float32; [B, ...]], latent tensor.
+        Returns:
+            sample: tf.Tensor, [tf.float32; [B, ...]], sample tensor.
+        """
+        sample = latent
+        for net in self.networks[::-1]:
+            sample = net.inverse(sample)
+        return sample
