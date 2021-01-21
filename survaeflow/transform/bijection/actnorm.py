503a153de86da5fed6da65fc5f40e64f3470b455
revsic <revsic99@gmail.com>
2021-01-22 00:50:01 +0900

    actnorm: Add activation normalization

---
+++ survaeflow/transform/bijection/actnorm.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+
+from .. import Transform
+
+
+class ActNorm(Transform):
+    """Activation normalization.
+    """
+    def __init__(self, axis=-1):
+        """Initializer.
+        Args:
+            axis: int, target axis.
+        """
+        super(ActNorm, self).__init__()
+        self.axis = axis
+        self.mult = 1.
+    
+    def build(self, input_shape):
+        """Generate the parameters.
+        Args:
+            input_shape: Tuple[int], input shape.
+        """
+        self.initialized = tf.Variable(False, dtype=tf.bool)
+        self.mu = tf.Variable(
+            tf.zeros(shape=(input_shape[self.axis],)))
+        self.log_sigma = tf.Variable(
+            tf.zeros(shape=(input_shape[self.axis],)))
+
+    def call(self, inputs):
+        """Run activation normalization and compute log-determinant of jacobian.
+        Args:
+            inputs: tf.Tensor, [tf.float32; [B, ..., C, ...]], input tensor.
+        Returns:
+            z: tf.Tensor, [tf.float32; [B, ..., C, ...]], normalized.
+            ldj: tf.Tensor, [tf.float32; []], log-determinant of jacobian.
+        """
+        if not self.initialized:
+            axis = list(range(len(inputs)))
+            axis = axis[:self.axis] + axis[self.axis + 1:]
+            # [C], [C]
+            mean, variance = tf.nn.moments(inputs, axis=axis)
+            # assign input stats
+            self.mu.assign(mean)
+            self.log_sigma.assign(0.5 * tf.math.log(variance + 1e-5))
+            self.initialized.assign(True)
+            # update multiplier
+            self.mult = tf.reduce_prod(tf.shape(inputs)[axis[1:]])
+        # [B, ..., C, ...]        
+        z = (inputs - self.mu) / tf.exp(self.log_sigma)
+        # []
+        ldj = tf.reduce_sum(-self.log_sigma) * self.mult
+        return z, ldj
+
+    def forward(self, inputs):
+        """Run activation normalization.
+        Args:
+            inputs: tf.Tensor, [tf.float32; [B, ..., C, ...]], input tensor.
+        Returns:
+            z: tf.Tensor, [tf.float32; [B, ..., C, ...]], latent vector.
+        Raises:
+            RuntimeError, if method `call` is not called before.
+        """
+        if not self.initialized:
+            raise RuntimeError('method `call` is not called before')
+        return (inputs - self.mu) / tf.exp(self.log_sigma)
+
+    def inverse(self, inputs):
+        """Run activation normalization.
+        Args:
+            inputs: tf.Tensor, [tf.float32; [B, ..., C, ...]], latent.
+        Returns:
+            tf.Tensor, [tf.float32; [B, ..., C, ...]], recovered tensor.
+        Raises:
+            RuntimeError, if method `call` is not called before.
+        """
+        if not self.initialized:
+            raise RuntimeError('method `call` is not called before')
+        return inputs * tf.exp(self.log_sigma) + self.mu
