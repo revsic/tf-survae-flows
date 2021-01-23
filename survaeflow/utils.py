1da397aea7ef526100abf55544cb42db343293bc
revsic <revsic99@gmail.com>
2021-01-23 20:03:49 +0900

    utils: Add pyplot_as_img

--- survaeflow/utils.py
+++ survaeflow/utils.py
@@ -1,3 +1,6 @@
+import io
+
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
@@ -23,3 +26,14 @@ def mean_except_batch(inputs):
     dim = len(inputs.shape)
     axis = list(range(dim))[1:]
     return tf.reduce_mean(inputs, axis=axis)
+
+
+def pyplot_as_img():
+    """Convert pyplot figure as image tensor.
+    Returns:
+        tf.Tensor, [_; [B, H, W, C]], image tensor.
+    """
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    return tf.image.decode_png(buf.getvalue(), channels=3)
