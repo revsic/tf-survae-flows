999f261baead6a87f4e6421cd7afb4f1e7485ca7
revsic <revsic99@gmail.com>
2021-01-23 16:33:46 +0900

    bijection: Add elemwise

--- survaeflow/transform/bijection/__init__.py
+++ survaeflow/transform/bijection/__init__.py
@@ -1,6 +1,7 @@
 from .actnorm import ActNorm
 from .affine_coupling import AffineCoupling
 from .conv1x1 import Conv1x1
+from .elemwise import Sigmoid, Logit
 from .scale import Scale
 
-__all__ = ['ActNorm', 'AffineCoupling', 'Conv1x1', 'Scale']
+__all__ = ['ActNorm', 'AffineCoupling', 'Conv1x1', 'Scale', 'Sigmoid', 'Logit']
