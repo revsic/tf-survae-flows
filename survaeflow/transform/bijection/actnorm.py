c56bfe9a1590b2a16867a62799645e60f88817d6
revsic <revsic99@gmail.com>
2021-01-23 20:03:41 +0900

    actnorm: Make initizlied as python member

--- survaeflow/transform/bijection/actnorm.py
+++ survaeflow/transform/bijection/actnorm.py
@@ -14,13 +14,13 @@ class ActNorm(Transform):
         super(ActNorm, self).__init__()
         self.axis = axis
         self.mult = 1.
+        self.initialized = False
     
     def build(self, input_shape):
         """Generate the parameters.
         Args:
             input_shape: Tuple[int], input shape.
         """
-        self.initialized = tf.constant(False, dtype=tf.bool)
         self.mu = tf.Variable(
             tf.zeros(shape=(input_shape[self.axis],)))
         self.log_sigma = tf.Variable(
@@ -47,9 +47,10 @@ class ActNorm(Transform):
             # assign input stats
             self.mu.assign(mean)
             self.log_sigma.assign(0.5 * tf.math.log(variance + 1e-5))
-            self.initialized = tf.constant(True, tf.bool)
             # update multiplier
             self.mult = tf.cast(tf.reduce_prod(tf.shape(inputs)[axis[1:]]), tf.float32)
+            # trig initialized
+            self.initialized = True
         # [B, ..., C, ...]        
         z = (inputs - self.mu) / tf.exp(self.log_sigma)
         # []
