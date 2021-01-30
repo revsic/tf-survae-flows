8101173749bac75465257c2202811572b1fef95d
revsic <revsic99@gmail.com>
2021-01-30 15:27:53 +0900

    dist: Support __all__

--- survaeflow/distribution/__init__.py
+++ survaeflow/distribution/__init__.py
@@ -1,17 +1,6 @@
-class Distribution:
-    """Distribution baseline.
-    """
-    def __init__(self):
-        """Initializer.
-        """
-        pass
+from .bernoulli import Bernoulli
+from .normal import Normal
+from .uniform import Uniform
 
-    def log_prob(self, sample, *args, **kwargs):
-        """Compute log-likelihood of given sample.
-        """
-        raise NotImplementedError('Distribution.log_prob is not implemented')
-
-    def sample(self, *args, **kwargs):
-        """Sample from distribution.
-        """
-        raise NotImplementedError('Distribution.sample is not implemented')
+__all__ = ['Bernoulli', 'Normal', 'Uniform'
+]
